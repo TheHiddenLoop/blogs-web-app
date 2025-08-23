1c17631d46e599af576c98c2b9a7d55c3500600d
Angesh0007 <chauhanangesh60@gmail.com>
2025-08-23 19:47:41 +0530

    New deploy

--- client/src/libs/axios.js
+++ client/src/libs/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://thinkscribe-f9go.onrender.com/api", 
-  withCredentials: true,
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api" 
+    : "/api",
+  withCredentials: true, 
 });
--- server/server.js
+++ server/server.js
@@ -1,29 +1,49 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { blogsRoutes } from "./routes/blogs.js";
-import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
-import signupRouter from "./routes/user.js"
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import { blogsRoutes } from "./routes/blogs.js";
+import signupRouter from "./routes/user.js";
 
 dotenv.config();
-const app=express();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.use(cookieParser());
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true              
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, 
+    credentials: true,
+  })
+);
 
-app.use("/api/blogs",blogsRoutes);
+app.use("/api/blogs", blogsRoutes);
 app.use("/api/auth", signupRouter);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running at ${process.env.PORT}=> http://localhost:${process.env.PORT}`);
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT} => http://localhost:${PORT}`);
+});
