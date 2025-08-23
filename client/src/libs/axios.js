90de615a77842bb5341d5c85b9295cb8a525c8c3
Angesh0007 <chauhanangesh60@gmail.com>
2025-08-23 20:25:12 +0530

    New deploy

--- client/src/libs/axios.js
+++ client/src/libs/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "http://localhost:3000/api" 
-    : "/api",
+    : "https://blogs-web-app-1.onrender.com/api",
   withCredentials: true, 
 });
