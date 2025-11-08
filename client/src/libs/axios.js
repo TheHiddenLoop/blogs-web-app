e14b0162ab6aef1c32c13704dc093265176b18cd
Angesh Chauhan <153787090+TheHiddenLoop@users.noreply.github.com>
2025-11-08 13:05:26 +0530

    Update axios base URL for production environment

--- client/src/libs/axios.js
+++ client/src/libs/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "http://localhost:3000/api" 
-    : "https://blogs-web-app-83x6.vercel.app/api",
+    : "https://blogs-web-app-1.onrender.com/api",
   withCredentials: true, 
 });
